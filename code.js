002105b746e93f900058c05f24fc159643a4d60f
Caleigh Christensen <christensen.caleigh@gmail.com>
2020-11-12 11:25:08 -0600

    add the first test and solution for into-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,17 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+/* can replace function statement with a function expression
+ and it still works because functions are first class citizens in JS
+*/
+
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+function helloWorld() {
+    return (typeof helloWorld, function() {
+
+    })
+}
